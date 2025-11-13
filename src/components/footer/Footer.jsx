97119c1c6304ae5bd4901fb36fda31a40ffdc9be
AbdullayevTemurmalik <tmaq77@gamil,com>
2025-11-13 14:37:36 +0500

    united-deal

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
   return (
     <div>
       <h1>Mana temur linki: https://www.figma.com/design/7UrBVasudHoAcmeQHhmqOy/E-commerce-Open-Source-UI-Design--Community-?node-id=0-1&p=f&t=Uni1Am4sfc7RW45m-0</h1>
+      <h1> mana okala meet link in google meet sizlar shuni ozini oliib uni google ni searche ga tashasela boldi :   https://meet.google.com/kai-qhxc-dtx </h1>
     </div>
   );
 };
