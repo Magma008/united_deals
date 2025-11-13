fa225964ad772c24c6cfe0e60fe2b19f610fa090
AbdullayevTemurmalik <tmaq77@gamil,com>
2025-11-13 14:31:21 +0500

    united-deal

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,6 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div>
-      <h1>salom men temur man </h1>
       <h1>Mana temur linki: https://www.figma.com/design/7UrBVasudHoAcmeQHhmqOy/E-commerce-Open-Source-UI-Design--Community-?node-id=0-1&p=f&t=Uni1Am4sfc7RW45m-0</h1>
     </div>
   );
